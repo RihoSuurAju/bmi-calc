b02dc07fcb717607e2aee6f22fa2ae883f80a955
Opilane TTHK <opilane@tthk.ee>
2020-01-07 09:41:49 +0200

    woo

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-let html = document.body.innerHTML;
 
-function calc() {
+function calc(){
 
-    const Age = document.getElementById("InputAge");
-    const Weight = document.getElementById("InputWeight");
-    const Height = document.getElementById("InputHeight");
-    const BMI = 1.3 * Weight / Height;
+    var Age = document.getElementById("InputAge").value;
+    console.log(Age)
+    var Weight = document.getElementById("InputWeight").value;
+    var Height = document.getElementById("InputHeight").value;
+    var BMI = Weight / (Height * Height);
 
-    if (Age > 20) {
+    if (Age > 20 && Age < 60 ) {
+
         if (BMI < 16) 
             alert(`Sinu BMI on ${BMI}. Olete ohtlikult alakaaluline.`);
-        if (BMI > 15.9) 
+        if (BMI >= 16 && BMI < 18.5) 
             alert(`Sinu BMI on ${BMI}. Olete alakaaluline.`);
-        if (BMI > 18.5) 
+        if (BMI >= 18.5 && BMI < 25.1) 
             alert(`Sinu BMI on ${BMI}. Teie kaal on normaalne.`);
-        if (BMI > 25) 
+        if (BMI >= 25.1 && BMI < 30.1) 
             alert(`Sinu BMI on ${BMI}. Olete ülekaaluline.`);
-        if (BMI > 30)
+        if (BMI >= 30 && BMI < 35.1)
             alert(`Sinu BMI on ${BMI}. Olete rasvunud.`);
-        if (BMI > 35)
+        if (BMI >= 35.1 && BMI <= 40.1)
             alert(`Sinu BMI on ${BMI}. Olete tugevalt rasvunud.`);
         if (BMI > 40.1)
             alert(`Sinu BMI on ${BMI}. Olete eluohtlikult rasvunud.`)
-    } else {
+    } 
+    else
         alert(`Vabandust, aga selle kalkulaatori kasutamiseks pead olema 20-60 aastat vana. Kalkulaator annab vaid selles vanusevahemikus õige tulemuse.`)
-    }
 }
